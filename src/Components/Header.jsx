cc98fe364098e947a45da32ca92a89ffaedb8794
lasha <lashatatu@gmail.com>
2025-03-12 00:20:29 +0400

    added task card and task list

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <header className="flex items-center justify-between">
-      <img src="../../public/logo.png" alt="momentum" className={'h-[38px]'}/>
+      <img src="../public/logo.png" alt="momentum" className={'h-[38px]'}/>
       <div className="flex gap-4">
         <div className="rounded-md border-[#8239EC] border-[1px] px-2 py-1">
           თანამშრომლის შექმნა

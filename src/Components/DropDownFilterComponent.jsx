0ec6cc9cc81288841221237febe1a1d9955e46d6
lasha <lashatatu@gmail.com>
2025-03-14 02:06:15 +0400

    moved all fetching to React Query and refactored code to make it more maintanable

--- src/Components/DropDownFilterComponent.jsx
+++ src/Components/DropDownFilterComponent.jsx
@@ -59,7 +59,7 @@ const DropdownContent = ({
             </div>
           ))
         )}
-        <div className="flex justify-end px-4 py-2">
+        <div className={"flex justify-end px-4 py-2"}>
           <button
             onClick={() => handleSelection(filterId)}
             className={
